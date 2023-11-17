c05a7c7257ab6ac20d5bb9d6e23fa70ace98487d
ncore-x <ncore@internet.ru>
2023-11-17 23:24:11 +1100

    Adding an Error Handler

--- src/operations/models.py
+++ src/operations/models.py
@@ -5,10 +5,10 @@ metadata = MetaData()
 operation = Table(
     "operation",
     metadata,
-    Column("id", Integer, primary_key=True),
+    Column("id", Integer, primary_key=True, autoincrement=True),
     Column("quantity", String),
     Column("figi", String),
-    Column("instrument_type", String, nullable=True),
+    Column("instrument_type", String, nullable=True, default=None),
     Column("date", TIMESTAMP),
     Column("type", String),
 )
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_async_session
 from operations.models import operation
 from operations.schemas import OperationCreate
+
 
 router = APIRouter(
     prefix="/operations",
@@ -13,14 +14,26 @@ router = APIRouter(
 
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+            "status": "success",
+            "data": [dict(r._mapping) for r in result],
+            "details": None
+        }
+    except Exception:
+        # Передать ошибку разработчикам
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
 
 
 @router.post("/")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(operation).values(**new_operation.dict())
+    stmt = insert(operation).values(**new_operation.model_dump())
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
